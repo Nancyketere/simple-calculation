0c576a2c406a895f8af32854577f86593311a3e9
Nancyketere <91130030+Nancyketere@users.noreply.github.com>
2021-10-12 17:10:57 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+/*
+Create a calculator program that will accept two numbers and compute the sum, difference
+ product, quotient and modulus of the 2 numbers.
+
+ INPUTS:
+ -sum
+ -diff
+ -pro
+ -quot
+ -mod
+
+ Computations:
+ - sum = num1 + num2
+ - diff = num1 - num2
+ - pro = num1 * num2
+ - quot = num1 / num2
+ - mod = num1 % num2
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Variable declaration
+    int num1, num2, mod;
+    float quot;
+
+    num1 = 200;
+    num2 = 56;
+  ;  sum = num1 + num2
+  ;diff = num1 - num2
+ ; pro = num1 * num2
+ ; quot = (float) num1 / num2
+ ; mod = num1 % num2
+
+ // output
+
+ // sum: result
+
+ // 3 + 2=8
+
+ printf("%d + %d = %d\n",num1,num2,num3);
+ printf("%d - %d = %d\n",num1,num2,num3);
+ printf("%d * %d = %d\n",num1,num2,num3);
+ printf("%d / %d = %d\n",num1,num2,num3);
+ printf("%d mod %d = %d\n",num1,num2,num3);
+
+;    return 0;
+}
